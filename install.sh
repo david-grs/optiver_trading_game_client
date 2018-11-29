bd1a076ee476f6db1abfa721e0dfd67c107e4be3
David Grs <david.a.grs@gmail.com>
2018-11-29 23:26:40 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
-#!/bin/bash
+#!/bin/bash -x
 
 sudo apt update
 sudo apt -y install g++ gdb cmake libboost-all-dev
 
 local_eth0_addr=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 sed -i s/LOCAL_ADDRESS/$local_eth0_addr/g src/autotrader.h
+
+mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2
 
