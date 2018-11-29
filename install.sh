ef79662adcfb18f573cd331d651c327344f99de7
David Grs <david.a.grs@gmail.com>
2018-11-29 23:23:49 +0100

    adding install.sh

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt -y install g++ gdb cmake libboost-all-dev
+
+local_eth0_addr=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+sed -i s/LOCAL_ADDRESS/$local_eth0_addr/g src/autotrader.h
+
