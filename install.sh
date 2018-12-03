ab51a48daa443ae494bd37db0b58ca993332af4a
David Grs <david.a.grs@gmail.com>
2018-12-03 10:09:41 +0100

    install.sh: arg1 username

--- install.sh
+++ install.sh
@@ -1,10 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash 
+
+if [ "$#" -ne 2 ]; then
+  echo "usage: $0 username"
+  exit 1
+fi
 
 sudo apt update
 sudo apt -y install g++ gdb cmake libboost-all-dev
 
 local_eth0_addr=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 sed -i s/LOCAL_ADDRESS/$local_eth0_addr/g src/autotrader.h
+
+username=$1
+sed -i s/USERNAME/$username/ src/autotrader_main.cc
 
 mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2
 
