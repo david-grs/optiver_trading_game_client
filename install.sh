c3d62fde7ea0e2f87df945f15d50b337c5e2dbaf
David Grs <david.a.grs@gmail.com>
2018-12-03 10:10:01 +0100

    install.sh: install pkg

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ if [ "$#" -ne 2 ]; then
   exit 1
 fi
 
-sudo apt update
-sudo apt -y install g++ gdb cmake libboost-all-dev
-
 local_eth0_addr=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 sed -i s/LOCAL_ADDRESS/$local_eth0_addr/g src/autotrader.h
 
 username=$1
 sed -i s/USERNAME/$username/ src/autotrader_main.cc
+
+sudo apt update
+sudo apt -y install g++ gdb cmake libboost-all-dev
 
 mkdir build && cd build && cmake -DCMAKE_BUILD_TYPE=Release .. && make -j2
 
