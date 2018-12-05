e75d560b0c4b48d5b5d056a89d9bca77ba42db32
David Grs <david.a.grs@gmail.com>
2018-12-05 14:14:27 +0100

    addng locked pnl

--- src/trading_result.h
+++ src/trading_result.h
@@ -10,13 +10,14 @@
 struct TradingResult
 {
 	double mPnL;
+	double mLockedPnL;
 	int mPosition;
 	int mTradedVolume;
 };
 
 inline std::ostream& operator<<(std::ostream& stream, const TradingResult& res)
 {
-	return stream << "pnl=" << res.mPnL << " position=" << res.mPosition << " traded_volume=" << res.mTradedVolume;
+	return stream << "pnl=" << res.mPnL << " locked_pnl=" << res.mLockedPnL << " position=" << res.mPosition << " traded_volume=" << res.mTradedVolume;
 }
 
 inline double CalculatePnL(const std::vector<std::pair<Price, Volume>>& trades, Price valuation)
@@ -27,6 +28,18 @@ inline double CalculatePnL(const std::vector<std::pair<Price, Volume>>& trades, 
 		const double price = p.first.mValue;
 		return pnl + volume * (valuation.mValue - price);
 	});
+}
+
+inline double CalculateLockedPnL(const std::vector<std::pair<Price, Volume>>& trades)
+{
+	const int buyVolume = std::accumulate(trades.cbegin(), trades.cend(), 0, [&](double x, const auto& p) { return x + std::max(0, (int)p.second.mValue); });
+	const int sellVolume = std::accumulate(trades.cbegin(), trades.cend(), 0, [&](double x, const auto& p) { return x - std::min(0, (int)p.second.mValue); });
+
+	const double buyAvg = buyVolume == 0 ? 0 : std::accumulate(trades.cbegin(), trades.cend(), .0, [&](double x, const auto& p) { return x + p.first.mValue * std::max(0, (int)p.second.mValue); }) / buyVolume;
+	const double sellAvg = sellVolume == 0 ? 0 : std::accumulate(trades.cbegin(), trades.cend(), .0, [&](double x, const auto& p) { return x - p.first.mValue * std::min(0, (int)p.second.mValue); }) / sellVolume;
+
+	const int closedVolume  = std::min(buyVolume, sellVolume);
+	return (sellAvg - buyAvg) * closedVolume;
 }
 
 inline int CalculatePosition(const std::vector<std::pair<Price, Volume>>& trades)
@@ -52,6 +65,7 @@ inline TradingResult CalculateTradingResult(const std::vector<std::pair<Price, V
 	return TradingResult
 	{
 		CalculatePnL(trades, valuation),
+		CalculateLockedPnL(trades),
 		CalculatePosition(trades),
 		CalculateTradedVolume(trades)
 	};
